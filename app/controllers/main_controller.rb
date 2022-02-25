2d5b822461fc5138ff550ff246e1b0c7c9d74106
robert-cummins <robert.k.cummins@gmail.com>
2022-02-25 18:27:42 +1300

    Add Login and Logout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
-    def index
-        flash[:notice] = "Logged in successfully"
-        flash[:alert] = "Invalid email or password"
-    end
+  def index
+		if session[:user_id]
+			#.find_by as oppossed to .find will not throw an error if user is not found (in case user was deleted while still logged in, in the session)
+			@user = User.find_by(id: session[:user_id])
+		end
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   #GET /about and use the about controller with index action
   get "about", to: "about#index"
 
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
