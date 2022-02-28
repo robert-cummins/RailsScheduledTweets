adcea43b8fa4ea0f5247b7b39a8836fe3497253e
robert-cummins <robert.k.cummins@gmail.com>
2022-02-28 14:40:30 +1300

    Add change password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+			#.find_by as oppossed to .find will not throw an error if user is not found (in case user was deleted while still logged in, in the session)
+			Current.user = User.find_by(id: session[:user_id])
+		end
+  end
+
+  def require_user_logged_in!
+    redirect_to sign_in_path, alert: "You must be logged in to do that" if Current.user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'password_reset/new'
+  get 'password_reset/create'
   #GET /about and use the about controller with index action
   get "about", to: "about#index"
+                                        # edit rails default path name "password" to edit_password
+  get "password", to: "password#edit", as: :edit_password
+  patch "password", to: "password#update"
 
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
